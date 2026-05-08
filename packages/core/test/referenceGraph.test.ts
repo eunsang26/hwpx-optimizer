bab30c81ee42e63763d0ffad12653cd106c213af
eunsang26 <eunsang26@gmail.com>
2026-05-08 16:32:14 +0900

    fix: separate manifest declarations from resource usage

--- packages/core/test/referenceGraph.test.ts
+++ packages/core/test/referenceGraph.test.ts
@@ -19,4 +19,21 @@ describe("buildReferenceGraph", () => {
     expect(graph.resources.get("BinData/image1.png")?.referenced).toBe(true);
     expect(graph.resources.get("BinData/image2.png")?.referenced).toBe(false);
   });
+
+  it("does not treat content.hpf manifest declarations as visible usage", async () => {
+    const fixture = await createHwpxFixture({
+      entries: {
+        "Contents/content.hpf": `<opf:package xmlns:opf="http://www.idpf.org/2007/opf/"><opf:manifest><opf:item id="image1" href="BinData/image1.png" media-type="image/png"/><opf:item id="image2" href="BinData/image2.png" media-type="image/png"/></opf:manifest></opf:package>`,
+        "Contents/section0.xml": `<root><hc:img binaryItemIDRef="image1" /></root>`,
+        "BinData/image1.png": Buffer.from("used"),
+        "BinData/image2.png": Buffer.from("declared but unused")
+      }
+    });
+
+    const pkg = await readHwpxPackage(fixture);
+    const graph = buildReferenceGraph(pkg);
+
+    expect(graph.resources.get("BinData/image1.png")?.referenced).toBe(true);
+    expect(graph.resources.get("BinData/image2.png")?.referenced).toBe(false);
+  });
 });
